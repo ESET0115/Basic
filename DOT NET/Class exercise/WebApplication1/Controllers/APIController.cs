f0ea0b86b1558449887fd1e2f93d559d7aff7887
piyush-esya <Piyush.Kumar@esyasoft.com>
2025-09-22 16:38:24 +0530

    React

---
+++ DOT NET/Class exercise/WebApplication1/Controllers/APIController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        //private List<string> students = new List<string>
+        //{
+        //    "abcd",
+        //    "sachin",
+        //    "rahul"
+        //};
+        private List<Dictionary<string, string>> student_dict = new List<Dictionary<string, string>>
+
+        {
+
+          new Dictionary<string, string>{{"id","1"},{"name","Sachin"}},
+
+          new Dictionary<string, string>{{"id","2"},{"name","Rahul"}},
+
+         };
+
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            return new JsonResult(student_dict);
+        }
+    }
+}
--- DOT NET/Class exercise/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ DOT NET/Class exercise/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86cd6f71972743d91d7c02800c8bea7a8757f549")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61dd68ec6048d055f60dd83ecd6f4d4e1eb0f76c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
